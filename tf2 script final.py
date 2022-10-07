40b7e2b33d976807556a86bac42a8e6a108d980f
AShyRag <93735653+AShyRag@users.noreply.github.com>
2022-10-07 19:08:57 +0530

    Update tf2 script final.py

--- tf2 script final.py
+++ tf2 script final.py
@@ -21,29 +21,37 @@ def on_message(ws, message):
     me1 = me["name"]    
     qualityy = me["quality"]
     quality = qualityy["id"]
-    particles = me["particle"]
-    particle = particles["name"]
-    effects = particles["id"]
-    user__id = sell["steamid"]
-    price = sell["currencies"]
-    key_price = price["keys"]
+    if(quality==5):
+
+        particles = me["particle"]
+        particle = particles["name"]
+        effects = particles["id"]
+        user__id = sell["steamid"]
+        price = sell["value"]
+        raw_price = price["raw"]
+        
    # effect_list = [ 37, 38,39, 40, 43, 44, 45, 46, 47,63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 107, 108, 109, 110, 114, 115, 116, 117, 118, 119, 120 , 122, 123, 124, 125, 126, 127, 128, 129, 130, 134, 135, 136, 137, 138, 158, 159, 160, 161, 164, 165,166, 167, 168  169, 170,171, 172, 173, 198, 199, 200, 201, 219, 220 ]
-    effect_list = ["37", "38","39", "40", "43", "44", "45", "46", "47","63", "64", "65", "66", "67", "68", "69", "70", "71", "72", "73", "74", "75", "76", "77",
-    "78", "79", "80", "81", "82", "83", "84", "85", "86", "87", "88", "89", "90", "107", "108", "109", "110", "114", "115", "116", "117", "118", "119", "120"
-    , "122", "123", "124", "125", "126", "127", "128", "129", "130", "134", "135", "136", "137", "138", "158", "159", "160", "161", "164", "165","166", "167", "168"
-    "169", "170","171", "172", "173", "198", "199", "200", "201", "219", "220"
-    ]
-    blacklist = ["76561198795826430", "76561198453530349" , "76561199072654974", "76561199106217370","76561198841279602"]
-    effect = str(effects)
-    user_id = str(user__id)
-    payload = {
-	    'content':  me1 ,
-        'price': key_price
+        effect_list = ["37", "38","39", "40", "43", "44", "45", "46", "47","63", "64", "65", "66", "67", "68", "69", "70", "71", "72", "73", "74", "75", "76", "77","78", "79", "80", "81", "82", "83", "84", "85", "86", "87", "88", "89", "90", "107", "108", "109", "110", "114", "115", "116", "117", "118", "119", "120", "122", "123", "124", "125", "126", "127", "128", "129", "130", "134", "135", "136", "137", "138", "158", "159", "160", "161", "164", "165","166", "167", "168"     "169", "170","171", "172", "173", "198", "199", "200", "201", "219", "220"]
+        blacklist = ["76561198795826430", "76561198453530349" , "76561199072654974", "76561199106217370","76561198841279602", "76561199102407070", "76561198850178158", 
+         "76561199118175648", "76561199111073161"
+        ]
+        effect = str(effects)
+        float_50 = float(45)
+        user_id = str(user__id)
+        payload = {
+	    'content':  f"@AShyRag#2695, item: {me1}. Item price is {raw_price/float_50}, intent is: {item_intent}"
           }
-    header = {
-	      'authorization': "Mjk2MjUxODg0OTkyNDYyODUw.GwhBAQ.KhHsOtLmzutOdUlwD3jTspzt2DksFuxHhoHBCI"
+        header = {
+            #Mjk2MjUxODg0OTkyNDYyODUw.G1rIDt.PCxnmRi3xkStFoRjas7EbInqI8j5G-ibIYAms8
+	      'authorization': "Mjk2MjUxODg0OTkyNDYyODUw.G1rIDt.PCxnmRi3xkStFoRjas7EbInqI8j5G-ibIYAms8"
             }
-    if(quality==5)and (effect in effect_list)and (key_price>50)and(user_id not in blacklist):
+        blacklist_names = ["Pumpkin Moon Beanie The All-Gnawing","Frozen Icefall Citizen Cane",  "Pumpkin Moon Towering Patch of Pumpkins", "Ominous Stare Beanie The All-Gnawing", "Pumpkin Moon Twisted Topper", "Holiday Horns Reindoonihorns"
+        "Special Snowfall Jolly Jingler", "Ardent Antlers Jolly Jingler"
+        
+        
+
+        ]
+        if (effect in effect_list)and (raw_price>2500)and(item_intent=="sell")and (user_id not in blacklist)and (me1 not in blacklist_names):
             r = requests.post("https://discord.com/api/v9/channels/1026466334759997525/messages", 
                   data = payload, headers=header)
 #blacklist:  76561198795826430 76561198453530349  76561199072654974 
@@ -58,7 +66,7 @@ def on_open(ws):
     print("Opened connection")
 
 if __name__ == "__main__":
-    websocket.enableTrace(True)
+    websocket.enableTrace(False)
     ws = websocket.WebSocketApp("wss://ws.backpack.tf/events",
                               on_message=on_message,
                               on_error=on_error,
